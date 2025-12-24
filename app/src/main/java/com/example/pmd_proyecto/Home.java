04e736667eb970f84fd30ffb3096c3a15c863974
MASKYX <yyx.yixiao.yao@gmail.com>
2025-12-24 20:24:31 +0100

    Fragments

---
+++ app/src/main/java/com/example/pmd_proyecto/Home.java
@@ -0,0 +1,55 @@
+package com.example.pmd_proyecto;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.menu);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment f;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) f = new HomeFragment();
+            else if (id == R.id.nav_retos) f = new RetosFragment();
+            else if (id == R.id.nav_problemas) f = new ProblemasFragment();
+            else f = new YoFragment();
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainerView, f)
+                    .commit();
+
+            return true;
+        });
+        // sección inicial
+        bottomNav.setSelectedItemId(R.id.nav_home);
+
+
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/pmd_proyecto/MainActivity.java
+++ app/src/main/java/com/example/pmd_proyecto/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pmd_proyecto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -42,6 +43,12 @@ public class MainActivity extends AppCompatActivity {
                 llamarAPI();
             }
         });
+
+        // Inicio parte para borrar: solo es para ir a la Home
+        Intent i = new Intent(MainActivity.this, Home.class);
+        startActivity(i);
+        // Fin parte para borrar
+
     }
 
     private void llamarAPI() {
