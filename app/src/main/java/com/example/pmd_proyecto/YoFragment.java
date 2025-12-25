3589c905094b736223fa20fcc21ca43475821eec
Shuhang <shuhang.pan@alumnos.upm.es>
2025-12-25 12:45:06 +0100

    SQLite para inicio/registro

--- app/src/main/java/com/example/pmd_proyecto/YoFragment.java
+++ app/src/main/java/com/example/pmd_proyecto/YoFragment.java
@@ -61,4 +61,5 @@ public class YoFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_yo, container, false);
     }
+    
 }
