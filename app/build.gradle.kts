affe2b0ec5a7cebf22e3bcae7c56d0c766bb672a
Qiu2025 <siyuanqiu123@gmail.com>
2025-12-26 21:43:36 +0100

    Fragment retos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.fragment)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
